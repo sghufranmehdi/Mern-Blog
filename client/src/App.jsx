b09689f1468aeb9e7ec356234be8c6b6326f38dd
sghufranmehdi <sgmehdi110@gmail.com>
2024-12-03 08:53:16 +0500

    routes and pages created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Projects from "./pages/Projects";
+import Dashboard from "./pages/Dashboard";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="sign-up" element={<SignUp />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="*"
+          element={
+            <h1 className="text-3xl flex items-center justify-center mt-[40vh]">
+              404 <br />
+              Page Not Found
+            </h1>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
